03e2a75b9be878de6d5fbbb1b34ba9bcf0b5fbc6
denulemos <denu.a.lemos@gmail.com>
2025-09-03 19:12:26 +0100

    delete unused dep

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import type {ReactNode} from 'react';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import {Redirect} from '@docusaurus/router';
 
 export default function Home(): ReactNode {
